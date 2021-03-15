1c31fe80cf01584bf7b6df32e03d37240a07c1a4
sebastianceloch <73536373+sebastianceloch@users.noreply.github.com>
2021-03-15 12:49:45 +0100

    Delete zadanie8.py

--- zadanie8.py
+++
@@ -1,6 +0,0 @@
-print('{1} {0}'.format('one', 'two'))
-print('{:10}'.format('test'))
-print('{:^20}'.format('test'))
-print('{:_<30}'.format('test'))
-print('{:.7}'.format('grzegrzółka'))
-print('{:+d}'.format(48))
