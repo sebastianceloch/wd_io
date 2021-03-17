931a8289b0b42b68f0defc790f5f859b09e11aee
sebastianceloch <CelejKontakt@gmail.com>
2021-03-17 17:48:00 +0100

    g

--- zadanie11.py
+++ zadanie11.py
@@ -1,18 +1,21 @@
 import sys
 h=int(input("Podaj wysokosc diamentu: "))
-s=h/2
-x=h
-r=1
-j=3
-while(x>s):
-    print(" "*int(j)+ "o"*int(r))
-    r+=2
-    j-=1
-    x-=1
-while(x>=0):
-    print(" "*int(j)+"o"*int(r))
-    r-=2
-    j+=1
-    x-=1
+if h<3 or h>10:
+    print("za malo lub za duza wysokosc")
+else:
+    s=h/2
+    x=h
+    r=1
+    j=3
+    while(x>s):
+        print(" "*int(j)+ "o"*int(r))
+        r+=2
+        j-=1
+        x-=1
+    while(x>=0):
+        print(" "*int(j)+"o"*int(r))
+        r-=2
+        j+=1
+        x-=1
 
 
