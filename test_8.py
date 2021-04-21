eef004953ba2140a63c02c40961563d21d33a444
sebastianceloch <73536373+sebastianceloch@users.noreply.github.com>
2021-04-21 11:54:12 +0200

    Delete test_8.py

--- test_8.py
+++
@@ -1,8 +0,0 @@
-def fun(tekst):
-    plik = input('wporwadz nazwe pliku ')
-    tekst = tekst.lower()
-    usun = tekst.replace(',','').replace('.','').split(' ')
-    with open(plik,'w') as f:
-        f.write('\n'.join([word for word in usun if usun.count(word) == 1]))
-tekst = 'Ala ma kota. ala.aa'
-fun(tekst)
