996764be11484b29595e729f8a9efd76084dcc11
sebastianceloch <73536373+sebastianceloch@users.noreply.github.com>
2021-04-21 11:54:08 +0200

    Delete test_7.py

--- test_7.py
+++
@@ -1,7 +0,0 @@
-import random
-def los(ile,przedzial):
-    random.seed()
-    num = [random.randint(*przedzial) for i in range(ile)]
-    print(num)
-    print(sum(num), sum(num)/len(num))
-los(5,(1,100))
