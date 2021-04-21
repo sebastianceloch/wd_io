f83bf17b5dcc508617c69c6f0531d249e35ad6ec
sebastianceloch <73536373+sebastianceloch@users.noreply.github.com>
2021-04-21 11:53:48 +0200

    Delete test_3.py

--- test_3.py
+++
@@ -1,5 +0,0 @@
-numbers = [i for i in range(1, 25 + 1)]
-print(numbers)
-numbers_two_level = [numbers[i * 5:i * 5 + 5] for i in range(int(len(numbers) / 5))]
-print(numbers_two_level)
-print(numbers_two_level[0][0])
